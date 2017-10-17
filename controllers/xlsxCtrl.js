c8a119a869255f7c1cc2dfabc01a1384a37d19ee
Robert Russo <rrusso44@icloud.com>
2017-10-16 20:14:21 -0400

    Removed duplicate emails

--- controllers/xlsxCtrl.js
+++ controllers/xlsxCtrl.js
@@ -5,15 +5,18 @@ module.exports = {
   parseEWS: function(path) {
     var buffer = xlsx.parse(fs.readFileSync(path));
     var students = [];
+    var emails = new Set();
     buffer[0].data.forEach(function(row, index) {
-      if (index > 0) {
-        students.push({
+      if (index > 0 && !emails.has(row[9])) {
+        emails.add(row[9]);
+        student = {
           'name': row[0].replace(/.*, /, ''),
           'reason': row[1],
           'hall': row[7],
           'room': row[8],
           'email': row[9]
-        });
+        };
+        students.push(student);
       }
     });
     return students;
